d5254bff899132ae51422b0142c1495c2b06231a
andrewjholbrook <aholbroo@g.ucla.edu>
2022-08-27 12:15:00 -0700

    slow progress

--- code/derivativeExperiment.R
+++ code/derivativeExperiment.R
@@ -5,13 +5,13 @@ setwd("~/expmDerivative/")
 ###### correct approximation to frechet derivative
 ###
 #
-n       <- 100
-Q       <- matrix(rexp(n^2),n,n)
+n       <- 3
+Q       <- matrix(rexp(n^2),n,n)#rexp(n^2)
 diag(Q) <- 0
 diag(Q) <- - rowSums(Q)
 
 E        <- matrix(0,n,n)
-E[3,1]   <- 1 
+E[3,2]   <- 1 
 frchtDrv <- expm::expmFrechet(A=t(Q),
                               E=E,
                               expm=FALSE)[[1]]
@@ -21,8 +21,8 @@ frstRdr
 norm(frstRdr-frchtDrv,type = "F")
 max(abs(frstRdr-frchtDrv))
 
-thrFrstRdr <- expm::expm(Q) %*% E # incorrect(?) approx
-thrFrstRdr[,1]
+thrFrstRdr <-Q%*% expm::expm(1*Q) %*% E # incorrect(?) approx
+thrFrstRdr[,2]
 sd(thrFrstRdr[,1])
 norm(thrFrstRdr-frchtDrv,type = "F")
 max(abs(thrFrstRdr-frchtDrv))
