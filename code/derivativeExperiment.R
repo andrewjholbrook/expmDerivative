46de75c366cccb847f59b3db20fc5123c42b55de
andrewjholbrook <aholbroo@g.ucla.edu>
2022-09-11 14:40:34 -0700

    numerics match theory in one way

--- code/derivativeExperiment.R
+++ code/derivativeExperiment.R
@@ -1,26 +1,26 @@
 setwd("~/expmDerivative/")
 
-# when do we get convergence?
-n       <- 3
-Q       <- matrix(rexp(n^2),n,n)
-diag(Q) <- 0
-diag(Q) <- - rowSums(Q)
-
-drv <- expm::expm(10*Q) #stationarity
-drv%*%Q
-
-# pseudoinverse
-eig.obj <- eigen(Q)
-inv.spctrm <- 1/eig.obj$values
-inv.spctrm[length(inv.spctrm)] <- 0
-pinv    <- eig.obj$vectors %*% diag(inv.spctrm) %*% solve(eig.obj$vectors)
-norm(pinv%*%Q%*%Q-Q) <= 0.00000001
-
-drv%*%Q
-
-drv%*%pinv
-
-norm(pinv%*%Q,type="F")
+# # when do we get convergence?
+# n       <- 3
+# Q       <- matrix(rexp(n^2),n,n)
+# diag(Q) <- 0
+# diag(Q) <- - rowSums(Q)
+# 
+# drv <- expm::expm(10*Q) #stationarity
+# drv%*%Q
+# 
+# # pseudoinverse
+# eig.obj <- eigen(Q)
+# inv.spctrm <- 1/eig.obj$values
+# inv.spctrm[length(inv.spctrm)] <- 0
+# pinv    <- eig.obj$vectors %*% diag(inv.spctrm) %*% solve(eig.obj$vectors)
+# norm(pinv%*%Q%*%Q-Q) <= 0.00000001
+# 
+# drv%*%Q
+# 
+# drv%*%pinv
+# 
+# norm(pinv%*%Q,type="F")
 
 # upper bound
 ub <- function(t, Q, Jij) {
@@ -33,14 +33,24 @@ ub <- function(t, Q, Jij) {
   return(norm(mat,type="F"))
 }
 
-n       <- 2
+nmrclDrvtv <- function(t,Q,E,eps=0.0001) {
+  out <- (expm::expm(t*(Q+eps*E/2))-expm::expm(t*(Q-eps*E/2)))/eps
+  return(out)
+}
+
+n       <- 200
 Q       <- matrix(rexp(n^2),n,n)
 diag(Q) <- 0
 diag(Q) <- - rowSums(Q)
 E        <- matrix(0,n,n)
-E[2,2]   <- 1 
+E[2,3]   <- 1 
 
-ub(100,Q,E)
+ub(1,Q,E)
+
+drv <- nmrclDrvtv(t=1,Q=Q,E=E,eps=0.00001)
+
+frstRdr <- expm::expm(1*Q) %*% E * 1 # correct approx
+norm(frstRdr-drv,type = "F")
 
 
 #
