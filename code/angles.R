6b695438f386df6356a6f861dfedf945bd1c896e
andrewjholbrook <aholbroo@g.ucla.edu>
2023-03-01 09:12:57 -0800

    xml file plus new manuscript folder

--- code/angles.R
+++ code/angles.R
@@ -115,6 +115,48 @@ gg2 <- ggplot(df,aes(x=t,y=Error,color=Dimension)) +
   theme_bw()
 gg2
 
+# df      <- data.frame(Error=numeric(),Dimension=character(),t=numeric())
+# 
+# 
+# for(n in 3:50) {
+#   for (t in seq(from=10^(-10),to=10,length.out=100)) {
+#     
+#     Q       <- matrix(rexp(n^2),n,n)
+#     diag(Q) <- 0
+#     diag(Q) <- - rowSums(Q)
+#     E       <- matrix(0,n,n)
+#     d <- sample(1:n,size=1)
+#     dprime <- sample((1:n)[-d],size=1)
+#     E[d,dprime]   <- 1
+#     E[d,d]  <- -1
+#     #E[2,2] <- 1
+#     drv <- blockwise(t=t,Q,E)
+#     frstRdr <- expm::expm(t*Q) %*% E *t # correct approx
+#     
+#     df <- rbind(df,
+#                 c(acos(sum(diag(frstRdr%*%drv))/
+#                          (norm(frstRdr,type="F")*norm(drv,type="F")))*(360/(2*pi)),
+#                   n,
+#                   t))
+#     
+#   }
+# }
+# 
+# colnames(df) <- c("Error","Dimension","t")
+# df$Dimension <- factor(df$Dimension)
+# df$Error <- as.numeric(df$Error)
+# df$t     <- as.numeric(df$t)
+# 
+# gg3 <- ggplot(df,aes(x=t,y=Error,color=Dimension)) +
+#   geom_line(size=1.5) +
+#   geom_hline(yintercept = 90) +
+#   # scale_y_log10() +
+#   ylab(" ") +
+#   coord_cartesian(ylim=c(0,100)) +
+#   ggtitle("Off/on-diagonal directional derivative vs 1st order approx.") +
+#   theme_bw()
+# gg3
+
 library(ggpubr)
 ggsave(ggarrange(gg,gg2,
                  ncol=2,nrow=1,
