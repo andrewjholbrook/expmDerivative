c0abbc1cd6f8fb60ae7d348fee50ac4bfa5aa69b
andrewjholbrook <aholbroo@g.ucla.edu>
2023-03-16 15:55:40 -0700

    post viz figs visualized

--- code/postDiagnostics.R
+++ code/postDiagnostics.R
@@ -9,3 +9,20 @@ effectiveSize(df[,9:11]) #        1053.3909        1342.5106         498.3175
 
 # random effects
 summary(effectiveSize(df[,12:1903]))
+
+
+# full run
+df <- read_table2("output/covid10Mar_285plusNYC.Ed.log", skip = 3)
+df <- df[df$state>=800000,]
+
+# global scale
+effectiveSize(df[,1904])
+
+# fixed effects
+effectiveSize(df[,9:11])
+
+# random effects
+summary(effectiveSize(df[,12:1903]))
+
+
+
